7665d797df698f9627414b6cff9933ea38bf088a
KBoulton <kim.s.boulton@gmail.com>
2015-12-09 12:21:06 +1100

    Movement Scripts

---
+++ Assets/Universal Helper Assets/Scripts/ObjectManipulation/DestroyAfterTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterTime : MonoBehaviour {
+
+	public float lifetime;
+
+	// Use this for initialization
+	void Start () {
+	
+		Destroy (gameObject, lifetime);
+
+	}
+	
+
+}
