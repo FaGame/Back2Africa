9b8e19d626a2131d1f0bf22118dab5adaf12a962
KBoulton <kim.s.boulton@gmail.com>
2015-12-09 17:17:00 +1100

    Scene Changes

---
+++ Assets/Scenes/Overworld/Scripts/ChangeSceneOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneOnCollision : MonoBehaviour {
+
+	public string levelName;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.collider.gameObject.tag == "Player")
+		{
+			Application.LoadLevel (levelName);
+		}
+	}
+
+	
+}
