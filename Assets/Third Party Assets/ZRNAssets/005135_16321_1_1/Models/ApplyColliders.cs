cd7705b5bfa71daccebaf2b713e3d885fa77603b
KBoulton <kim.s.boulton@gmail.com>
2015-12-09 13:46:00 +1100

    Collider City

---
+++ Assets/Third Party Assets/ZRNAssets/005135_16321_1_1/Models/ApplyColliders.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyColliders : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		Transform[] transforms = gameObject.GetComponentsInChildren<Transform>();		
+
+		foreach(Transform t in transforms)
+		{
+			GameObject go = t.gameObject;
+			
+			if(go.GetComponent<MeshRenderer>() != null)
+			{
+				MeshCollider col = go.AddComponent <MeshCollider>() as MeshCollider;
+			}
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
