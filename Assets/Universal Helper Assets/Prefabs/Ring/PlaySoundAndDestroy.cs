1d969521eea1830250ede207246525cd9e831582
KBoulton <kim.s.boulton@gmail.com>
2015-12-09 14:15:38 +1100

    Added Rings

---
+++ Assets/Universal Helper Assets/Prefabs/Ring/PlaySoundAndDestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundAndDestroy : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.gameObject.tag == "Player")
+		{
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.PlayOneShot (audio.clip);
+			Destroy(GetComponentInChildren<MeshRenderer>());
+			Destroy (GetComponent<BoxCollider>());
+			Destroy (GetComponent<Light>());
+			Destroy (gameObject, 1.0f);
+		}
+	}
+}
